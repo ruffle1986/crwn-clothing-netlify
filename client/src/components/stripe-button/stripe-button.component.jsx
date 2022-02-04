6889109b9a42d4586996216a92ac0e2bf7779395
ruffle1986 <2196208+ruffle1986@users.noreply.github.com>
2022-02-04 09:00:00 +0100

    add backend to stripe properly

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,4 +1,5 @@
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -6,8 +7,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51KNwEfJduOcWdo5w88tVTVn02GgQNBZSjEvfw6SYTb0r5jlXXR6Lca50MkvNQku9RNowM9wXfNVSnSsu8nXt2Agh00EsobwHOy';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: '/payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card.',
+        );
+      });
   };
 
   return (
